8003215693cfecd18b77e6c1904e08b2f484cdd0
Bünyamin Bilen KARATAŞ <108085663+bunyaminbilenkaratas@users.noreply.github.com>
2024-06-21 06:25:53 +0300

    All tasks done

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\StatsController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 
 /*
 |--------------------------------------------------------------------------
